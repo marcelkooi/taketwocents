75c80c1abe62e43fe95a69f26df330da5b6967b4
Marcel <1marcelkooi@gmail.com>
2015-10-17 15:41:40 -0400

    create user feature

--- web/app/controllers/users_controller.rb
+++ web/app/controllers/users_controller.rb
@@ -1,5 +1,40 @@
 class UsersController < ApplicationController
 
+	def show
+		@user = User.find(params[:id])
+	end
 
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			redirect_to stack_entry_path(1, 1)
+		else
+			redirect_to root_path
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+
+		if @user.update(user_params)
+			redirect_to @user
+		else
+			redirect_to root_path
+		end
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:username, :password, :ethnicity, :age, :gender)
+		end
 
 end
--- web/app/models/user_response.rb
+++ web/app/models/user_response.rb
@@ -3,7 +3,7 @@ class UserResponse < ActiveRecord::Base
 	belongs_to :entry
 	belongs_to :user
 
-	# validates_uniqueness_of :user, scope: :entry
+	validates_uniqueness_of :user, scope: :entry
 
 	validates :user, presence: true
 	validates :entry, presence: true
